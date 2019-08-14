91470adbe6edae35a923b76697988031b55e182f
Rhys Lloyd <rhys.lloyd@empired.com>
2019-08-14 21:51:23 +0930

    Facilitate custom config for theme

--- index.js
+++ index.js
@@ -1,30 +1,64 @@
 // index.js
-'use strict';
+"use strict";
 
-const mandelbrot = require('@frctl/mandelbrot');
-
-/*
- * Configure the theme
- */
-const subTheme = mandelbrot({
-    nav: ['docs', 'components'],
-    skin: 'white',
-    styles: ['default', '/mono-theme/webui.min.css', '/webui-overrides/overrides.css'],
-    panels: ['view', 'context', 'html'],
-    favicon: '/mono-theme/favicon.ico'
-});
+const mandelbrot = require("@frctl/mandelbrot");
+const _ = require("lodash");
 
 /*
- * Specify a template directory to override any view templates
+ * Required config overrides incoming options
+ * Note: styles array is merged
+ *
  */
-subTheme.addLoadPath(__dirname + '/webui-overrides');
+const requiredConfig = {
+  skin: "white",
+  styles: [
+    "default",
+    "/mono-theme/webui.min.css",
+    "/webui-overrides/overrides.css"
+  ]
+};
 
 /*
- * Specify the static assets directory that contains the custom stylesheet.
+ * Optional config can be overridden
  */
-subTheme.addStatic(__dirname + '/assets', '/mono-theme');
+const optionalConfig = {
+  nav: ["docs", "components"],
+  panels: ["view", "context", "html"],
+  favicon: "/mono-theme/favicon.ico"
+};
 
 /*
  * Export the customised theme instance so it can be used in Fractal projects
  */
-module.exports = subTheme;
+module.exports = function(options) {
+  /*
+   * Configure the theme
+   */
+  const styles = _.uniq(requiredConfig.styles, options.styles || []);
+
+  const config = _.defaultsDeep(
+    {
+      styles: styles
+    },
+    requiredConfig,
+    _.clone(options || {}),
+    optionalConfig
+  );
+
+  const subTheme = mandelbrot(config);
+
+  /*
+   * Specify a template directory to override any view templates
+   */
+  subTheme.addLoadPath(__dirname + "/webui-overrides");
+
+  /*
+   * Specify the static assets directory that contains the custom stylesheet.
+   */
+  subTheme.addStatic(__dirname + "/assets", "/mono-theme");
+
+  /*
+   * Provide for further configuration
+   */
+  return subTheme;
+};
